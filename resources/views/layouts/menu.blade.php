b21e66625bce5efc4dfdae297fadbe535195d386
Bruno Ferreira <fs.brunoferreira@gmail.com>
2017-04-17 13:55:48 +0000

    Laravel 5.3 with Infyom Generator and AdminLTE Template

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,7 @@
+<li class="header">Navegação Principal</li>
+{{--<li class="{{ Request::is('grupos*') ? 'active' : '' }}">--}}
+    {{--<a href="{!! route('grupos.index') !!}">--}}
+        {{--<i class="icon ion-university"></i>--}}
+        {{--<span> Grupos</span>--}}
+    {{--</a>--}}
+{{--</li>--}}
